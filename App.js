f92fdd2d724970f95eddc17a76ca41e7a7ee25e1
jpshirey5 <jayshirey@icloud.com>
2024-02-08 11:07:20 -0500

    Functionally changing to the selected photos

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View, Text } from 'react-native';
 import ImageViewer from './Components/imageViewer';
+import * as ImagePicker from 'expo-image-picker';
 import Button from "./Components/button";
 
 const placeholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select an image.')
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={placeholderImage} />
+        <ImageViewer placeholderImageSource={placeholderImage}
+        selectedImage = {selectedImage}/>
       </View>
       <View style={styles.footerContainer}>
-        <Button label={"Choose a Photo"} />
+        <Button theme={"primary"} label={"Choose a Photo"} onPress={pickImageAsync} />
         <Button label={"Use this Photo"} />
       </View>
       <StatusBar style="auto" />
