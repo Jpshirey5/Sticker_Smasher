8207a96ae75eeffcbb9a4e13500cc3edfb151aa8
jpshirey5 <jayshirey@icloud.com>
2024-02-08 00:42:08 -0500

    background image and buttons created

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+import ImageViewer from './Components/imageViewer';
+import Button from "./Components/button";
+
+const placeholderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={placeholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label={"Choose a Photo"} />
+        <Button label={"Use this Photo"} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+});
